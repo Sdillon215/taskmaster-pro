06564013c44e41e619b2aec827df1e275c66e2fc
Sean <sdillon215@gmail.com>
2021-07-09 13:15:48 -0700

    datpicker added

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,12 +100,21 @@ $(".list-group").on("click", "span", function () {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 0,
+    onClose: function() {
+      // when calender is closed force a change event o the datpicker
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -245,6 +254,10 @@ $("#trash").droppable({
   out: function (event, ui) {
     console.log("out");
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 0
 });
 
 // load tasks for the first time
